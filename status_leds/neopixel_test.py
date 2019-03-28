b42669e2cd92ca083e1c9631020ee2e2725dda7f
Dharmin <dharmingeo@gmail.com>
2019-03-28 14:41:55 +0100

    Added test leds option in the shell script

--- status_leds/neopixel_test.py
+++ status_leds/neopixel_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import test
 import board
 import neopixel
-from led_lights import LedLights
 
 if __name__ == '__main__':
     pixels = neopixel.NeoPixel(board.D18, 12)
-    LedLights.set_status_1_and_remaining(pixels)
+    pixels.fill((255, 255, 255))
+    time.sleep(3)
+    pixels.fill((0, 0, 0))
--- status_leds/run_neopixel.sh
+++ status_leds/run_neopixel.sh
@@ -4,5 +4,10 @@
 export BLINKA_FORCEBOARD=RASPBERRY_PI_B_PLUS
 export BLINKA_FORCECHIP=BCM2XXX
 
-# run the actual code
-python3 ropod_led_controller.py
+if [ "$1" = "test" ]; then
+	# run test program
+	python3 neopixel_test.py
+else
+	# run the actual code
+	python3 ropod_led_controller.py
+fi
